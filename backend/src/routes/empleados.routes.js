dab8b893f1372ca7173b165fb436ac07daa7941d
DaniDev <programandopanconqueso@hotmail.com>
2026-04-29 10:42:00 -0400

    feature: add services and middleware for images

--- backend/src/routes/empleados.routes.js
+++ backend/src/routes/empleados.routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import * as empleadosController from '../controllers/empleados.controller.js';
 import { validateSchema } from '../middlewares/validate.middleware.js';
 import { createEmpleadosSchema, updateEmpleadosSchema } from '../schemas/empleados.schema.js';
+import { upload } from '../middlewares/upload.middleware.js';
 import { protect } from '../middlewares/auth.middleware.js';
 import { tenantMiddleware } from '../middlewares/tenant.middleware.js';
 import { checkPermission } from '../middlewares/permission.middleware.js';
@@ -13,8 +14,8 @@ router.use(tenantMiddleware);
 
 router.get('/', checkPermission('empleados', 'see'), empleadosController.getEmpleados);
 router.get('/:id', checkPermission('empleados', 'see'), empleadosController.getEmpleadoById);
-router.post('/', checkPermission('empleados', 'create'), validateSchema(createEmpleadosSchema), empleadosController.createEmpleado);
-router.put('/:id', checkPermission('empleados', 'update'), validateSchema(updateEmpleadosSchema), empleadosController.updateEmpleado);
+router.post('/', checkPermission('empleados', 'create'), upload.single('foto'), validateSchema(createEmpleadosSchema), empleadosController.createEmpleado);
+router.put('/:id', checkPermission('empleados', 'update'), upload.single('foto'), validateSchema(updateEmpleadosSchema), empleadosController.updateEmpleado);
 router.delete('/:id', checkPermission('empleados', 'delete'), empleadosController.deleteEmpleado);
 
 export default router;
